f1d1e4773c7e51a91707f428278d45c6d9ff7888
imabuzerkhan <abuzerk41@gmail.com>
2024-07-08 20:49:48 +0545

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaGithub } from 'react-icons/fa'; 
+
+const Navbar = () => {
+    const [state, setState] = useState(false);
+
+    const navigation = [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
+        { title: "Skills", path: "/skills" }
+    ];
+
+    useEffect(() => {
+        document.onclick = (e) => {
+            const target = e.target;
+            if (!target.closest(".menu-btn")) setState(false);
+        };
+    }, []);
+
+    return (
+        <nav className={`  bg-[#1A202C] pb-2 md:text-sm ${state ? "shadow-lg rounded-xl border mx-2 mt-2 md:shadow-none md:border-none md:mx-2 md:mt-0" : ""}`}>
+            <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
+                <div className="flex items-center justify-between py-5 md:block">
+                    <Link href="/">
+                        <Image
+                            src="/logo.svg"
+                            width={120}
+                            height={50}
+                            alt="Float UI logo"
+                        />
+                    </Link>
+                    <div className="md:hidden">
+                        <button className="menu-btn text-white hover:text-gray-400"
+                            onClick={() => setState(!state)}
+                        >
+                            {
+                                state ? (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                                    </svg>
+                                ) : (
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                    </svg>
+                                )
+                            }
+                        </button>
+                    </div>
+                </div>
+                <div className={`flex-1 items-center mt-8 md:mt-0 md:flex ${state ? 'block' : 'hidden'} `}>
+                    <ul className="justify-center items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
+                        {
+                            navigation.map((item, idx) => {
+                                return (
+                                    <li key={idx} className="text-white hover:text-gray-400">
+                                        <Link href={item.path} className="block">
+                                            {item.title}
+                                        </Link>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                    <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
+                        <Link href="/" className="block text-white hover:text-gray-400">
+                            View Resume
+                        </Link>
+                        <Link href="https://github.com/imabuzerkhan" className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex">
+                            GitHub
+                            <FaGithub className="w-5 h-5" />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,7 @@
+const navigation = [
+  { title: "Home", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+  { title: "Skills", path: "/skills" }
+];
+export default navigation
