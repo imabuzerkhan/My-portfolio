60bbefff88a3ffbd786388b4a5ab517ce800ab9e
imabuzerkhan <abuzerk41@gmail.com>
2024-07-09 11:42:05 +0545

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/Hero";
+import Project from "@/components/Project";
 
 export default function Home() {
   return (
    <>
    <HeroSection></HeroSection>
+   <Project/>
    </>
   );
 }
