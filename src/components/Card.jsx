94c8266dab43ef3d7cb4fe17f14aceca3ceebce6
imabuzerkhan <abuzerk41@gmail.com>
2024-07-10 11:28:20 +0545

    adding react icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import { FaHtml5 } from "react-icons/fa";
-
+import { FaHtml5 , FaCss3Alt , FaReact , FaPython , FaJava  , FaGitSquare    } from "react-icons/fa";
+import { RiJavascriptFill , RiTailwindCssFill } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
 const Card = () => {
   return (
    <>
@@ -9,6 +10,45 @@ const Card = () => {
     <div className="card">
       <div><FaHtml5 /></div>
       <span>Html</span>
+    </div>
+    <div className="card">
+      <div><FaCss3Alt /> </div>
+      <span>Css</span>
+    </div>
+    <div className="card">
+      <div><RiJavascriptFill /> </div>
+      <span>Javascript</span>
+
+    </div>
+    <div className="card">
+      <div><FaReact /> </div>
+      <span>React</span>
+      
+    </div>
+    <div className="card">
+      <div><TbBrandNextjs /> </div>
+      <span>Next.js</span>
+      
+    </div>
+    <div className="card">
+      <div><RiTailwindCssFill /> </div>
+      <span>Tailwind Css</span>
+      
+    </div>
+    <div className="card">
+      <div><FaPython /> </div>
+      <span>Python</span>
+      
+    </div>
+    <div className="card">
+      <div><FaJava /> </div>
+      <span>Java</span>
+      
+    </div>
+    <div className="card">
+      <div><FaGitSquare /> </div>
+      <span>Git</span>
+      
     </div>
    </div>
    </>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import Card from './Card'
 
 const Skills = () => {
   return (
-    <div>
+    <div className='bg-[#1A202C] mt-10 px-8 lg:px-40' >
        <h1 className="text-3xl md:text-5xl font-extrabold mb-4">
         Skills <span className="text-[#4FD1C5]">Section</span>
       </h1>
