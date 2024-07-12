33be1ee25a0d53350fef755e69e907c6ccc3215d
imabuzerkhan <abuzerk41@gmail.com>
2024-07-12 21:22:41 +0545

    details updated

--- src/components/Cntact-details.jsx
+++ src/components/Cntact-details.jsx
@@ -8,7 +8,7 @@ const Cntact_details = () => {
         {/*------------------ First details---------------------------------- */}
 
     <div className="Contact-number flex items-center justify-center gap-3 ">
-<FaPhoneAlt  className="bg-white px-3 py-3 text-[#1eac9e] rounded-full text-5xl  "  />
+<FaPhoneAlt  className="bg-white px-4 py-4 text-7xl lg:px-3 lg:py-3 text-[#1eac9e] rounded-full lg:text-5xl  "  />
 <div className="Contact-text flex items-center flex-col ">
   <h3 className="text-2xl font-mono font-medium " >Phone Number</h3>
   <h5 className="text-lg text-[#4FD1C5] " >+977 9804395755</h5>
@@ -18,7 +18,7 @@ const Cntact_details = () => {
     {/*------------------ Second details---------------------------------- */}
     
     <div className="Contact-Gmail flex items-center justify-center gap-3 ">
-<MdOutgoingMail  className="bg-white px-3 py-3 text-[#1eac9e] rounded-full text-5xl "  />
+<MdOutgoingMail  className="bg-white px-4 py-4 lg:px-3 lg:py-3 text-[#1eac9e] rounded-full text-7xl lg:text-5xl "  />
 <div className="Gmail-text flex  flex-col ">
   <h3 className="text-2xl font-mono font-medium " >Gmail</h3>
   <h5 className="text-lg text-[#4FD1C5] " >Abuzerzak41@gmail.com</h5>
@@ -29,7 +29,7 @@ const Cntact_details = () => {
  {/*------------------ Third details---------------------------------- */}
     
  <div className="Contact-Gmail flex items-center justify-center gap-3 ">
-<FaAddressBook  className="bg-white px-3 py-3 text-[#1eac9e] rounded-full text-5xl  "  />
+<FaAddressBook  className="bg-white px-4 py-4 lg:px-3 lg:py-3 text-[#1eac9e] rounded-full lg:text-5xl text-7xl  "  />
 <div className="Gmail-text flex  flex-col ">
   <h3 className="text-2xl font-mono font-medium " >Address</h3>
   <h5 className="text-lg text-[#4FD1C5] " >Biratnagr 06,Nepal</h5>
