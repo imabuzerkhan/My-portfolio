0e6c5f7f17153954779ea510cb25bee30752a954
imabuzerkhan <abuzerk41@gmail.com>
2024-07-12 21:11:43 +0545

    new commit

--- src/components/ContactBanner.jsx
+++ src/components/ContactBanner.jsx
@@ -7,7 +7,7 @@ const ContactBanner = () => {
   return (
    <>
    <div className="new-banner w-full bg-[#2f3747] mt-20 flex items-center">
-  <div className="banner w-full px-8 sm:px-3 lg:px-36 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5 sm:gap-10 xl:gap-36 py-4">
+  <div className="banner w-full px-8 sm:px-3 lg:px-32 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5 sm:gap-10 xl:gap-36 py-4">
     <Cntact_details />
   </div>
 </div>
