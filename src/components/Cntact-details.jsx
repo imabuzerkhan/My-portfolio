0af4f6b6dc7e878c8d043a4806356c5d187fa35d
imabuzerkhan <abuzerk41@gmail.com>
2024-07-15 17:08:49 +0545

    px-2

--- src/components/Cntact-details.jsx
+++ src/components/Cntact-details.jsx
@@ -18,7 +18,7 @@ const Cntact_details = () => {
     {/*------------------ Second details---------------------------------- */}
     
     <div className="Contact-Gmail flex items-center justify-center gap-3 ">
-<MdOutgoingMail  className="bg-white px-4 py-4 lg:px-1 lg:py-1 text-[#1eac9e] rounded-full text-7xl lg:text-5xl "  />
+<MdOutgoingMail  className="bg-white px-4 py-4 lg:px-2 lg:py-1 text-[#1eac9e] rounded-full text-7xl lg:text-5xl "  />
 <div className="Gmail-text flex  flex-col ">
   <h3 className="text-2xl font-mono font-medium " >Gmail</h3>
   <h5 className="text-lg text-[#4FD1C5] " >Abuzerzak41@gmail.com</h5>
